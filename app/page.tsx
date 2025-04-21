5ca1b98b941c4acba867b186dad6677ef0ab4e02
codendive <codendive@gmail.com>
2025-04-21 13:46:27 +0200

    Fix layout for cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import TodoList from '@/components/TodoList'
-// #F0D9B1
+
 const HomePage = async () => {
   return (
-    <div className='container p-6 mx-auto'>
+    <div className='max-w-6xl'>
       <h1 className='mb-4 text-3xl font-bold text-center'>📋 My Todos</h1>
-
       <TodoList />
     </div>
   )
